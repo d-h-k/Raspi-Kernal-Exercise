57a3491042f8e5f49d331c42b3f3427d71ca1a75
DHkim <ultraraptor@naver.com>
2020-06-08 20:29:22 +0900

    update header

---
+++ K-HEADER.sh
@@ -0,0 +1,4 @@
+apt-get install -y git bison flex gcc vim make​
+apt-get install -y gcc-arm-linux-gnueabihf​
+apt-get install -y linux-headers-$(uname –r)​
+​apt-get install -y raspberrypi-kernel-headers
