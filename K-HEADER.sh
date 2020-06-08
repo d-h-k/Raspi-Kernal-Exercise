80d0540cbe87978112fb7ee462a0952f4ce62b01
DongHun Kim <31065684+d-h-k@users.noreply.github.com>
2020-06-08 20:42:56 +0900

    Update K-HEADER.sh

--- K-HEADER.sh
+++ K-HEADER.sh
@@ -1,6 +1,9 @@
 apt-get install -y git bison flex gcc vim make
+
 apt-get install -y gcc-arm-linux-gnueabihf
-#apt-get install -y gcc-arm-linux-gnueabi
+#sudo apt-get install -y gcc-arm-none-eabi
+
 apt-get install -y linux-headers-4.19.118-v7l+
 #sudo apt-get install -y linux-headers-4.18.0-3-common
+
 apt-get install -y raspberrypi-kernel-headers
